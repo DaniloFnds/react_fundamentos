72226c1b0d09c9933418b5f6d58a2b303059662a
Danilo <danilofg0@gmail.com>
2022-05-11 20:03:06 -0300

    importar um css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import ReactDom from 'react-dom'
 import React from 'react'
 
+//como importar um css
+import './index.css'
+
 //para funcionar um html dentro do react precisamos importar o react
 ReactDom.render(<div>Ola react</div>, document.getElementById('root'))
+
