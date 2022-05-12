2871a10d4bde94c908b36b015c1e87262a38ba75
Danilo <danilofg0@gmail.com>
2022-05-11 22:07:15 -0300

    fragment

---
+++ src/components/basicos/Fragmento.js
@@ -0,0 +1,8 @@
+export default function Fragmento() {
+
+    return (// passar um <></> é um React.Fragment para encapsular alguns elementos
+        <>
+            <h2>Cuidado com esse erro</h2>
+        </>
+    )
+}
