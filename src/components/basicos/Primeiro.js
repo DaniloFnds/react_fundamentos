6c58dc146a9a83351296e51c9b59efbe4bfc3f8d
Danilo <danilofg0@gmail.com>
2022-05-11 21:42:13 -0300

    component parametro

---
+++ src/components/basicos/Primeiro.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Primeiro() {
+    //se coloca os parentes para poder colocar mais elementos para ser retornado
+    const msg = 'Seja Bem vindo'
+    return (
+        <div>
+            <h2>Primeiro Component</h2>
+            <p>{msg}</p>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ import React from 'react'
 //como importar um css
 import './index.css'
 
-//para funcionar um html dentro do react precisamos importar o react
-ReactDom.render(<div>Ola react</div>, document.getElementById('root'))
+//importar um component
+import Primeiro from './components/basicos/Primeiro'
+import ComParametro from "./components/basicos/ComParametro";
 
+
+//para funcionar um html dentro do react precisamos importar o react
+// ReactDom.render(<div>Ola react</div>,
+//     document.getElementById('root'))
+
+//colocando o component para ser renderizado
+// ReactDom.render(<div><Primeiro></Primeiro></div>, document.getElementById('root'))
+
+
+//passando parametros para um component
+ReactDom.render(<div>
+    <Primeiro/>
+    <ComParametro titulo="Segundo COmponssent" subtitulo="Subtitulssso"/>
+</div>, document.getElementById('root'))
