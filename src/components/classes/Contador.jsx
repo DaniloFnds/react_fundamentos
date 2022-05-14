13ad92c42fab6b23e9119222f751df4a4340571d
Danilo <danilofg0@gmail.com>
2022-05-14 14:12:20 -0300

    finalizacao

--- src/components/classes/Contador.jsx
+++ src/components/classes/Contador.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Display from "./Display";
+import Botoes from "./Botoes";
+import PassoForm from "./PassoForm";
 
 class Contador extends React.Component {
     state = {
-        numero: this.props.numeroInicial || 0,
-        passo: this.props.passoInicial || 5,
+        numero: this.props.numeroInicial || 0, passo: this.props.passoInicial || 5,
     }
 
     /* constructor(props) {
@@ -15,29 +17,29 @@ class Contador extends React.Component {
     //para resolver a questao de conseguir atualizar o compoentn e conseguri usar o this dentro do compoent, é usar uma funcao arrow
     //aqui seria criar um increment recebe uma funcao () =>
     increment = () => {
+        console.log('asdas')
         //a forma de atualizar o estado do component é chamado o setState
         this.setState({
             numero: this.state.numero + this.state.passo
         })
     }
 
-    handleChange = (e) => {
+    setPasso = (e) => {
+        console.log(e)
         this.setState({
-            passo: Number(e.target.value)
+            passo: e
         })
     }
 
     render() {
-        return (
-            <div>
-                <h2>Contador</h2>
-                <p>{this.state.numero}</p>
-                <input type="text" value={this.state.passo} onChange={this.handleChange}/>
-                <button onClick={this.increment}>+</button>
-                {/*outra forma de chamar o metodo é passar uma arrow funciton*/}
-                {/*<button onClick={_ => this.increment()}>++</button>*/}
-            </div>
-        );
+        return (<div>
+            <h2>Contador</h2>
+            <Display numero={this.state.numero}></Display>
+            <PassoForm passo={this.state.passo} setPasso={this.setPasso}></PassoForm>
+            <Botoes incrementar={this.increment}></Botoes>
+            {/*outra forma de chamar o metodo é passar uma arrow funciton*/}
+            {/*<button onClick={_ => this.increment()}>++</button>*/}
+        </div>);
     }
 }
 
